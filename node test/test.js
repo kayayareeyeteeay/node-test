1a5a7c827dce378c82d750310920f5396085861c
kayayareeyeteeay <karitaarpad@gmail.com>
2025-03-11 14:21:22 +0100

    Projektek

---
+++ node test/test.js
@@ -0,0 +1,27 @@
+const sql = require('mssql/msnodesqlv8');
+
+var config = {
+    server: 'localhost\\SQLEXPRESS',
+    database: 'FundWiseDb',
+    user: 'sa',
+    password: '1A2w3e4F',
+    driver: 'msnodesqlv8',
+    options:{
+        trustServerCertificate: true
+    }
+};
+
+async function createDatabase(){
+    try{
+        await sql.connect(config);
+
+        const request = new sql.Request();
+        const createDbQuery = `CREATE DATABASE FundWiseDb`;
+        await request.query(createDbQuery) ;
+       }
+       catch(err){
+        console.error("error creating db:", err)
+       }
+};
+
+module.exports = {createDatabase, sql};
