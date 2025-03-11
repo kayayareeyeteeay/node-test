26407ca3b081c63c716bfd8705f6febf84da80e6
kayayareeyeteeay <karitaarpad@gmail.com>
2025-03-11 16:10:05 +0100

    Szerverhez kapcsolódás, API-k integrálása

--- test.js
+++ test.js
@@ -2,6 +2,28 @@ const sql = require('mssql/msnodesqlv8');
 
 var config = {
     server: 'localhost\\SQLEXPRESS',
+    database: 'FundWise',
+    user: 'sa',
+    password: '1A2w3e4F',
+    driver: 'msnodesqlv8',
+    options:{
+        trustServerCertificate: true
+    }
+};
+
+sql.connect(config, function(err){
+    if(err)console.log(err);
+    var request = new sql.Request();
+    request.query("select * from dbo.investments",function(err,records){
+        if(err)console.log(err);
+        else console.log(records);
+    })
+})
+
+
+
+var config = {
+    server: 'localhost\\SQLEXPRESS',
     database: 'FundWiseDb',
     user: 'sa',
     password: '1A2w3e4F',
@@ -11,7 +33,7 @@ var config = {
     }
 };
 
-async function createDatabase(){
+/*async function createDatabase(){
     try{
         await sql.connect(config);
 
@@ -27,3 +49,9 @@ async function createDatabase(){
 module.exports = {createDatabase, sql};
 
 
+
+
+module.exports = {createDatabase, sql};
+
+
+*/
